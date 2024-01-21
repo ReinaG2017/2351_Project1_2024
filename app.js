72e7227065961ddde19b428243472d5a6f5c8b30
ReinaG2017 <89498550+ReinaG2017@users.noreply.github.com>
2024-01-20 23:50:55 -0600

    added CON/DOC/INNER

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log('How did you get into codeing as a profession?');
+document.write("<strong>Looking forward to learning more JavaScript this semester!</strong>");
+document.getElementById("demo").innerHTML = 2021+3;
