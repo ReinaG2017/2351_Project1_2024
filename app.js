77f0822edf5f11cda74146c8dd40bfdbf0297b40
ReinaG2017 <89498550+ReinaG2017@users.noreply.github.com>
2024-01-28 20:35:05 -0600

    Mod 2 - Variables

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-console.log('How did you get into codeing as a profession?');
-document.write("<strong>Looking forward to learning more JavaScript this semester!</strong>");
-document.getElementById("demo").innerHTML = 2021+3;
+let fullName = "Reina Guajardo"; // variable value
+console.log(fullName);
+
+let age = 3;
+
+function1();
+
+function function1(){ 
+console.log(age);
+} // global variable
+
+{
+    let year = 2024;
+  }
+  // block scope
+
+  let y = 100 + 20; //mathematical operation variable
+  console.log(y);
+
+
+  let text1 = "25";
+  let text2 = "5";
+  let result = text1 < text2; //string variable
+    console.log("My favorite numbers a is " + text1 + text2);
+
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(numbers);
