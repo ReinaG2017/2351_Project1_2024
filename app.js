14d0eadf8a851c908ab3593424c4844ecba0a2ae
ReinaG2017 <89498550+ReinaG2017@users.noreply.github.com>
2024-02-04 14:21:38 -0600

    Functions and parameters Mod 3.4

--- app.js
+++ app.js
@@ -27,3 +27,36 @@ console.log(age);
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(numbers);
+
+//Assignment 3.4 - Functions and Parameters Below,
+
+const food = [
+  { name: 'Pepsi', calories: 150 },
+  { name: 'Twinkie', calories: 135 },
+  { name: 'Apple', calories: 95 },
+];
+
+food.forEach(function(food) {
+  console.log(food.name + ' - ' + food.calories);
+}); //Function : display name and calories 
+// parementsn In are name and calories
+//parameter Out will display both together with a hypen in between
+
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  country:"Norway"
+};
+
+console.log(person.firstName + ' - ' + person.lastName+ ' - ' + person.country);
+//object with Property Create an object with multiple properties
+
+//Event is shown on the HTML page
+
+
+let text5 = "I love cats. Cats are very easy to love. Cats are very popular."
+text5 = text5.replaceAll("Cats","Dogs");
+text5 = text5.replaceAll("cats","dogs");
+
+console.log(text5); //Method
